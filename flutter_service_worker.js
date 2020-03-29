c741cee195e1a85dbe83ca07ecde2f4e0abb109c
InspireUI <minhcasi@gmail.com>
2020-03-29 09:24:01 +0700

    build web

---
+++ flutter_service_worker.js
@@ -0,0 +1,42 @@
+'use strict';
+const CACHE_NAME = 'flutter-app-cache';
+const RESOURCES = {
+  "/index.html": "e6e25ef07ab061f3396db68372e4cc59",
+"/main.dart.js": "7fb941e856dea29efa97828b2dbcf9ca",
+"/favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"/icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"/icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+"/manifest.json": "ceeb714d2e5d68a0cd3b4efbc6a46994",
+"/assets/LICENSE": "954706ef951b9553ecb95d1a66a0f589",
+"/assets/AssetManifest.json": "dc0cb557f5c8211b5754d79a95725179",
+"/assets/FontManifest.json": "01700ba55b08a6141f33e168c4a6c22f",
+"/assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "115e937bb829a890521f72d2e664b632",
+"/assets/fonts/MaterialIcons-Regular.ttf": "56d3ffdef7a25659eab6a68a3fbfaf16",
+"/assets/assets/logo.png": "8bdb7cf9f117476a9473fa6a4940bcfa",
+"/assets/assets/img2.png": "d409ef31d54935557bb7a83b8c071ff9",
+"/assets/assets/img1.png": "51478f7f36ee4e4061296de27771de13"
+};
+
+self.addEventListener('activate', function (event) {
+  event.waitUntil(
+    caches.keys().then(function (cacheName) {
+      return caches.delete(cacheName);
+    }).then(function (_) {
+      return caches.open(CACHE_NAME);
+    }).then(function (cache) {
+      return cache.addAll(Object.keys(RESOURCES));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function (response) {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
